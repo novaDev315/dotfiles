ae7a4edfcb321997bf54dfaa160d2152e9be966e
Mohamed <mohamed@novadev.tech>
2024-05-20 10:22:43 +0400

    Modify: make sepeart installer using nix package manager

---
+++ install-nix.sh
@@ -0,0 +1,51 @@
+# install nix package manager:
+ curl -L https://nixos.org/nix/install | sh
+
+# source nix:
+ . ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages:
+ nix-env -iA \
+        nixpkgs.zsh \
+        nixpkgs.antibody \
+        nixpkgs.git \
+        nixpkgs.neovim \
+        nixpkgs.yarn \
+        nixpkgs.fzf \
+        nixpkgs.stow \
+        nixpkgs.tmux \
+        nixpkgs.bat \
+        nixpkgs.direnv \
+        nixpkgs.ripgrep \
+        nixpkgs.zoxide \
+        nixpkgs.tldr \
+        nixpkgs.libnotify \
+        nixpkgs.lsd \
+        nixpkgs.fd \
+        nixpkgs.duf
+
+# create the config folders 
+mkdir -p ~/.config/{nvim, bat}
+# stow the configs files
+ stow git -t ~
+ stow zsh -t ~
+ stow nvim -t ~/.config/nvim/
+ stow bat -t ~/.config/bat/
+ stow tmux -t ~
+ stow mime -t ~
+
+# install node version manager 
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# add zsh to valid login shells:
+ command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell:
+ sudo sed s/required/sufficient/g -i /etc/pam.d/chsh # fix chsh PAM Authentication failure
+ sudo chsh -s $(which zsh) $USER
+
+# bundle zsh plugins
+ antibody bundle < $HOME/.zsh_plugins.txt > $HOME/.zsh_plugins.sh
+
+# install neovim plugins
+ nvim --headless "+Lazy! sync" +qa
