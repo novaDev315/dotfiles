2d60e1ec60c1b385d9d6eed2de3ff34b78619b96
Mohamed <mohamed@novadev.tech>
2024-05-23 22:12:55 +0400

    Update stow command to use configs folder

--- install-nix.sh
+++ install-nix.sh
@@ -27,16 +27,16 @@
 # create the config folders 
 mkdir -p ~/.config/{nvim, bat}
 # stow the configs files
- stow git -t ~
- stow zsh -t ~
- stow nvim -t ~/.config/nvim/
- stow bat -t ~/.config/bat/
- stow tmux -t ~
- stow mime -t ~
+ [ -d configs/git ] && stow -d configs git -t ~
+ [ -d configs/zsh ] && stow -d configs zsh -t ~
+ [ -d configs/nvim ] && stow -d configs nvim -t ~/.config/nvim/
+ [ -d configs/bat ] && stow -d configs bat -t ~/.config/bat/
+ [ -d configs/tmux ] && stow -d configs tmux -t ~
+ [ -d configs/mime ] && stow -d configs mime -t ~
 
 # install node version manager 
  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
+ 
 # add zsh to valid login shells:
  command -v zsh | sudo tee -a /etc/shells
 
